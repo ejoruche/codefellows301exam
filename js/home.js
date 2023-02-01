b42d59ccb0b940c40b3e277796c41700e21033ec
EJ Oruche <engineering@x2n.co>
2023-01-31 17:14:57 -0800

    clean up gutters, handle button based on input

--- js/home.js
+++ js/home.js
@@ -1,27 +1,34 @@
+function Task(taskDesc) {
+    this.taskDesc = taskDesc;
+    this.createDate = Date.now();
+    this.isCompleted = false;
+}
+
+function handleAddTask() {
+    const inputValue = document.getElementById('taskinput').value;
+    localStorage.setItem(new Task(inputValue));
+
+    displayInfoText(`'${inputValue}' was added successfully`);
+    document.getElementById('taskinput').value = '';
+}
+
+function displayInfoText(message) {
+    const infoText = document.getElementById('infotext');
+    infoText.textContent = message;
+    infoText.style.visibility = 'visible';
+    setTimeout(() => {
+        infoText.style.visibility = 'hidden';
+    }, '1500');
+}
+
 window.onload = () => {
-    function Task(taskDesc) {
-        this.taskDesc = taskDesc;
-        this.createDate = Date.now();
-        this.isCompleted = false;
-    }
-
-    function handleAddTask() {
-        const inputValue = document.getElementById('taskinput').value;
-        localStorage.setItem(new Task(inputValue));
-
-        displayInfoText(`'${inputValue}' was added successfully`);
-        document.getElementById('taskinput').value = '';
-    }
-
-    function displayInfoText(message) {
-        const infoText = document.getElementById('infotext');
-        infoText.textContent = message;
-        infoText.style.visibility = 'visible';
-        setTimeout(() => {
-            infoText.style.visibility = 'hidden';
-        }, '1500');
-    }
-
     const addTaskButton = document.getElementById('addtaskbutton');
+    const addTaskInput = document.getElementById('taskinput')
+
+    addTaskInput.addEventListener("input", (event) => {
+        if (!event.target.value) {
+            addTaskButton.disabled = true;
+        } else addTaskButton.disabled = false;
+    })
     addTaskButton.addEventListener('click', handleAddTask);
 };
