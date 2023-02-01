e379b5b0ec871dfa36d28da79565432d1a7bbffc
EJ Oruche <engineering@x2n.co>
2023-01-31 16:14:03 -0800

    reordered code

--- js/home.js
+++ js/home.js
@@ -1,25 +1,27 @@
-function handleAddTask() {
-    const inputValue = document.getElementById('taskinput').value;
-    console.log(new Task(inputValue));
-    displayInfoText(`'${inputValue}' was added successfully`);
-    document.getElementById('taskinput').value = '';
-}
 window.onload = () => {
+    function Task(taskDesc) {
+        this.taskDesc = taskDesc;
+        this.createDate = Date.now();
+        this.isCompleted = false;
+    }
+
+    function handleAddTask() {
+        const inputValue = document.getElementById('taskinput').value;
+        localStorage.setItem(new Task(inputValue));
+
+        displayInfoText(`'${inputValue}' was added successfully`);
+        document.getElementById('taskinput').value = '';
+    }
+
+    function displayInfoText(message) {
+        const infoText = document.getElementById('infotext');
+        infoText.textContent = message;
+        infoText.style.visibility = 'visible';
+        setTimeout(() => {
+            infoText.style.visibility = 'hidden';
+        }, '1500');
+    }
+
     const addTaskButton = document.getElementById('addtaskbutton');
     addTaskButton.addEventListener('click', handleAddTask);
 };
-
-function Task(taskDesc) {
-    this.taskDesc = taskDesc;
-    this.createDate = Date.now();
-    this.isCompleted = false;
-}
-
-function displayInfoText(message) {
-    const infoText = document.getElementById('infotext');
-    infoText.textContent = message;
-    infoText.style.visibility = 'visible';
-    setTimeout(() => {
-        infoText.style.visibility = 'hidden';
-    }, '1500');
-}
