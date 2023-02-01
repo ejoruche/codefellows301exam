81da65668b62ce9dbdb8ff03cd1117f5d99e67a8
EJ Oruche <engineering@x2n.co>
2023-01-31 16:02:04 -0800

    added confirm of task stored; & clearing input

--- js/home.js
+++ js/home.js
@@ -1,6 +1,8 @@
 function handleAddTask() {
     const inputValue = document.getElementById('taskinput').value;
     console.log(new Task(inputValue));
+    displayInfoText(`'${inputValue}' was added successfully`);
+    document.getElementById('taskinput').value = '';
 }
 window.onload = () => {
     const addTaskButton = document.getElementById('addtaskbutton');
@@ -11,4 +13,13 @@ function Task(taskDesc) {
     this.taskDesc = taskDesc;
     this.createDate = Date.now();
     this.isCompleted = false;
+}
+
+function displayInfoText(message) {
+    const infoText = document.getElementById('infotext');
+    infoText.textContent = message;
+    infoText.style.visibility = 'visible';
+    setTimeout(() => {
+        infoText.style.visibility = 'hidden';
+    }, '1500');
 }
